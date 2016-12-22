3a8aaf7a529d81763ca5ea77da228b76090c5cfa
baumjr <mauricio.baumjr@gmail.com>
2016-12-22 18:50:41 -0200

    ..

--- sigasapp/config/environments/development.rb
+++ sigasapp/config/environments/development.rb
@@ -57,8 +57,8 @@ Rails.application.configure do
       :address              => "smtp.gmail.com",
       :port                 => 587,
       :domain               => 'gmail.com',
-      :user_name            => 'noreply.sigas@gmail.com',
-      :password             => '****',
+      :user_name            => ENV['MAILER_EMAIL'],
+      :password             => ENV["MAILER_PASSWORD"],
       :authentication       => 'plain',
       :enable_starttls_auto => true 
     }
